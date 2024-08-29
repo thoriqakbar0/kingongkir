90a8c18efd803016a04b18a600be678e95f8a489
thoriqakbar0 <41317726+thoriqakbar0@users.noreply.github.com>
2024-08-29 21:45:48 +0700

    init lucia

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     hello world
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Link href="/" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Login with Google
+      </Link>
     </main>
   );
 }
